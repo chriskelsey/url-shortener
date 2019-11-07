53e2fcee0c62bb094fe63cc52c8d1aa9ae33835b
chriskelsey <chriskelsey.net@gmail.com>
2019-11-07 10:42:48 -0800

    Cleaning up the code

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,16 @@
+//jQuery function to return either an error or shortened url on form submit
 $('button').on('click', function(e) {
+	$('.errorMessage, .short').remove();
 	const urlObj = {};
 	const newUrl = $('#url').val();
 	if(!newUrl){
+		$('form').before('<p class="errorMessage">Please Enter a valid url</p>');
 		$('.errorMessage').show();
 	} else {
 		urlObj.url = newUrl;
 		$.post('/newUrl', urlObj)
 		 .done(function(data){
-		 	$('form').after('<h2>Your Shortened URL is: </h2><p>http:localhost:9000/'+data._id+'</p>');
+		 	$('form').after('<div class="short"><h2>Your Shortened URL is: </h2><p>http:localhost:9000/'+data._id+'</p></div>');
 		 });
 	}
 	e.preventDefault();
